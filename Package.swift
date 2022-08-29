0b8bb4376d7f572678c540e3b8348c104efaf156
YK Poh <yungkien.poh@grabtaxi.com>
2022-08-29 15:52:55 +0800

    Update library name

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Jumio",
-            targets: ["Jumio"]),
+            name: "LineFinder",
+            targets: ["LineFinder"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
