3d81d34038b02ea48e0cdbadea0ace7f0bb7daf5
YK Poh <yungkien.poh@grabtaxi.com>
2022-08-29 15:02:46 +0800

    Add initial Swift Package Files

---
+++ Sources/JumioNFC/Jumio.xcframework/ios-x86_64-simulator/Jumio.framework/Headers/Jumio.h
@@ -0,0 +1,17 @@
+//
+//  Jumio.h
+//
+//  Copyright © 2022 Jumio Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jumio.
+FOUNDATION_EXPORT double JumioVersionNumber;
+
+//! Project version string for Jumio.
+FOUNDATION_EXPORT const unsigned char JumioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jumio/PublicHeader.h>
+
+
